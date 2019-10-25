8477336d6e25c29fb8e614f9b71b86be99be1d99
Flodin-Christoffer <flodin.christoffer@gmail.com>
2019-10-25 12:01:06 +0200

    added paintReservation and alertsucces

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 
 // const Calender = props => (
 class Calender extends Component {
+  state = {
+    alert: ""
+  };
   // runs on mount
   componentDidMount = async () => {
     // marking checkbox that has reservations
@@ -11,7 +14,7 @@ class Calender extends Component {
   };
 
   makeReservation = e => {
-    if (document.getElementById(e.target.id).checked == false) {
+    if (document.getElementById(e.target.id).checked === false) {
       if (
         window.confirm(
           "Are you sure you want to let go of this great laundry time?"
@@ -21,83 +24,118 @@ class Calender extends Component {
     } else {
       if (window.confirm("Are you sure you wish to reserve this laundry time?"))
         console.log(e.target.id);
+      const laundryTime = {
+        week: 1,
+        day: "Monday",
+        timeZone: 1,
+        userName: "chris",
+        email: "chris@de.se"
+      };
+      this.paintReservation(laundryTime);
     }
   };
+
+  paintReservation(laundryTime) {
+    let id =
+      "Weeknumber:" +
+      laundryTime.week +
+      "Day:" +
+      laundryTime.day +
+      "Timezone:" +
+      laundryTime.timeZone;
+    console.log(id);
+    document.getElementById(id).checked = true;
+    this.setState({
+      alert: laundryTime.userName + " has made a reservation "
+    });
+    setTimeout(() => this.setState({ alert: "" }), 8000);
+  }
 
   render() {
     return (
       // all css classes found in app.css
-      <div className="grid-5">
-        {this.props.days.map(day => {
-          return (
-            <div key={day} className="card text-center border-radius scaleCard">
-              <h2 className="mb-15"> {day} </h2>
+      <div>
+        {/* shows alert if no text is entered */}
+        {this.state.alert ? (
+          <div className="alert alert-success border-radius">
+            <i className="fas fa-info-circle"></i> {this.state.alert}
+          </div>
+        ) : null}
+        <div className="grid-5">
+          {this.props.days.map(day => {
+            return (
+              <div
+                key={day}
+                className="card text-center border-radius scaleCard"
+              >
+                <h2 className="mb-15"> {day} </h2>
 
-              <h4 className="text-left mb-10 grid-2">
-                06:00-09:00{" "}
-                <input
-                  className="checkBox"
-                  type="checkbox"
-                  value="06:00-09:00"
-                  id={
-                    "Weeknumber:" +
-                    this.props.week +
-                    "Day:" +
-                    day +
-                    "Timezone:" +
-                    1
-                  }
-                  onClick={this.makeReservation}
-                ></input>
-                09:00-12:00
-                <input
-                  className="checkBox"
-                  type="checkbox"
-                  value="09:00-12:00"
-                  id={
-                    "Weeknumber:" +
-                    this.props.week +
-                    "Day:" +
-                    day +
-                    "Timezone:" +
-                    2
-                  }
-                  onClick={this.makeReservation}
-                ></input>
-                12:00-15:00
-                <input
-                  className="checkBox"
-                  type="checkbox"
-                  value="12:00-15:00"
-                  id={
-                    "Weeknumber:" +
-                    this.props.week +
-                    "Day:" +
-                    day +
-                    "Timezone:" +
-                    3
-                  }
-                  onClick={this.makeReservation}
-                ></input>
-                15:00-18:00
-                <input
-                  className="checkBox"
-                  type="checkbox"
-                  value="15:00-18:00"
-                  id={
-                    "Weeknumber:" +
-                    this.props.week +
-                    "Day:" +
-                    day +
-                    "Timezone:" +
-                    4
-                  }
-                  onClick={this.makeReservation}
-                ></input>
-              </h4>
-            </div>
-          );
-        })}
+                <h4 className="text-left mb-10 grid-2">
+                  06:00-09:00{" "}
+                  <input
+                    className="checkBox"
+                    type="checkbox"
+                    value="06:00-09:00"
+                    id={
+                      "Weeknumber:" +
+                      this.props.week +
+                      "Day:" +
+                      day +
+                      "Timezone:" +
+                      1
+                    }
+                    onClick={this.makeReservation}
+                  ></input>
+                  09:00-12:00
+                  <input
+                    className="checkBox"
+                    type="checkbox"
+                    value="09:00-12:00"
+                    id={
+                      "Weeknumber:" +
+                      this.props.week +
+                      "Day:" +
+                      day +
+                      "Timezone:" +
+                      2
+                    }
+                    onClick={this.makeReservation}
+                  ></input>
+                  12:00-15:00
+                  <input
+                    className="checkBox"
+                    type="checkbox"
+                    value="12:00-15:00"
+                    id={
+                      "Weeknumber:" +
+                      this.props.week +
+                      "Day:" +
+                      day +
+                      "Timezone:" +
+                      3
+                    }
+                    onClick={this.makeReservation}
+                  ></input>
+                  15:00-18:00
+                  <input
+                    className="checkBox"
+                    type="checkbox"
+                    value="15:00-18:00"
+                    id={
+                      "Weeknumber:" +
+                      this.props.week +
+                      "Day:" +
+                      day +
+                      "Timezone:" +
+                      4
+                    }
+                    onClick={this.makeReservation}
+                  ></input>
+                </h4>
+              </div>
+            );
+          })}
+        </div>
       </div>
       // );
     );
